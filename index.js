2468de99b976230dac96c6b944049d22bd736c94
Jeremy R Geddes <jgeddes@us.ibm.com>
2017-01-03 19:37:29 -0500

    allow datastore to be run separately

--- index.js
+++ index.js
@@ -5,111 +5,58 @@
 
 'use strict';
 
+var express = require('express');
 var Promise = require('bluebird');
+var ds = require('./datastore.js');
 var logger = require('apiconnect-cli-logger/logger.js')
-        .child({ loc: 'microgateway:datastore' });
-var forever = require('forever-monitor');
-var path = require('path');
-
-var child;
+        .child({ loc: 'microgateway-datastore:microgw' });
+var app = express();
 var server;
-
-exports.start = function(fork) {
+exports.start = function(port) {
   return new Promise(function(resolve, reject) {
-    if (fork) {
-      child = new (forever.Monitor)(path.resolve(__dirname, 'server/server.js'), {
-        max: 10,
-        args: [],
-        fork: true,
-        killTree: true,
+    ds.start(process.env.NODE_ENV === 'production')
+      .catch(function(err) {
+        logger.debug('datastore failed to start: ', err);
+        ds.stop()
+          .then(function() {
+            reject(err);
+          });
       });
-
-      child.on('restart', function() {
-        logger.debug('datastore restarting, count=' + child.times);
-      });
-
-      child.on('exit', function() {
-        logger.debug('datastore exited');
-      });
-
-      var dataStorePort;
-      var https = false;
-      var loaded = false;
-      child.on('message', function(msg) {
-        if (msg.DATASTORE_PORT != null) {
-          dataStorePort = msg.DATASTORE_PORT;
-        }
-        if (msg.LOADED != null) {
-          if (!msg.LOADED) {
-            reject(Error('failed to load datastore'));
-            return;
-          }
-          loaded = true;
-        }
-        if (msg.https != null) {
-          https = msg.https;
-        }
-        // waiting for both events, seen scenario where
-        // they come out of order..
-        if (loaded && dataStorePort) {
-          child.removeAllListeners('message');
-          process.env.DATASTORE_PORT = dataStorePort;
-          resolve(https);
-        }
-      });
-
-      child.on('stderr', function(data) {
-        process.stderr.write(data);
-      });
-
-      child.on('stdout', function(data) {
-        process.stdout.write(data);
-      });
-
-      child.on('disconnect', function() {
-        logger.exit(2);
-      });
-
-      child.start();
-
-    } else {
-      process.send = function(msg) {
-        var https = false;
-        if (msg.https != null) {
-          https = msg.https;
-        }
-
-        if (msg.LOADED != null) {
-          process.send = function() {};
-          if (!msg.LOADED) {
-            reject(Error('failed to load datastore'));
-          } else {
-            resolve(https);
-          }
-        }
-      };
-      server = require('./server/server.js');
-      server.start();
-    }
   });
 };
 
 exports.stop = function() {
   return new Promise(function(resolve, reject) {
-    if (child) {
-      child.on('exit', function() {
-        child = undefined; // reset child
-        resolve();
-      });
-      child.stop();
-    } else if (server) {
-      server.close(function() {
-        server = undefined; // reset server
-        resolve();
-      });
-    } else {
-      resolve();
-    }
+    ds.stop()
+      .then(function() {
+        if (server) {
+          server.close(function() {
+            resolve();
+          });
+        } else {
+          resolve();
+        }
+      })
+      .catch(reject);
   });
 };
 
+exports.app = app;
+
+if (require.main === module) {
+  exports.start().then(function() {});
+}
+
+var ctx_config = {
+  request: {
+    contentTypeMaps: [
+      { 'application/json': [ '*/json', '+json', '*/javascript' ] },
+      { 'application/xml': [ '*/xml', '+xml' ] } ],
+    bodyFilter: {
+      DELETE: 'ignore',
+      GET: 'ignore',
+      HEAD: 'ignore',
+      OPTIONS: 'ignore' } },
+  system: {
+    datetimeFormat: 'YYYY-MM-DDTHH:mm:ssZ',
+    timezoneFormat: 'Z' } };
